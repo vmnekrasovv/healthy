6156db5453a536b429414707bbdddf792d4dafd8
test <test@gmail.com>
2024-02-04 11:59:55 +0300

    Подключил slick slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,39 @@
 (function($){
 	$(document).ready(function(){ 
 
+		$('.about-slider').slick({
+			speed: 700,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			swipeToSlide: true,
+			autoplay: true,
+			autoplaySpeed: 7000,
+			prevArrow: "<div class='about-slider__arrow prev'></div>",
+			nextArrow: "<div class='about-slider__arrow next'></div>",
+			responsive: [
+		
+				/*{
+				  breakpoint: 768,
+				  settings: {
+				    slidesToShow: 2,
+				    arrows: false
+				  }
+				},
+				{
+				  breakpoint: 576,
+				  settings: {
+				    slidesToShow: 1,
+				    arrows: false
+				  }
+				}*/
+
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			]
+		});
+
+
 		/*$('.scroll-down').on('click', function(){
 			$('html, body').animate({
 				scrollTop: $('.navbar').offset().top
